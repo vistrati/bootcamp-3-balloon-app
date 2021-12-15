aa0ffc4d012687beebcf6d9084b27d472c915a24
Victor Istrati <v.istrati@developmentaid.org>
2021-12-15 13:44:05 +0200

    Create models for blog part

---
+++ app/Models/Article.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'author_id',
+        'image',
+        'published_at',
+        'excerpt',
+        'category_id',
+        'seo_title',
+        'seo_description',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(BlogCategory::class, 'category_id');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(BlogTag::class);
+    }
+}
---
+++ app/Models/BlogCategory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BlogCategory extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'seo_title',
+        'seo_description',
+    ];
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
+}
