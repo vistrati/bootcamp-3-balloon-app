a6fed3943d883dead3091127c2d959d6a5d12d14
Egor Boaghi <e.boaghi@developmentaid.org>
2022-01-10 18:52:24 +0200

    Add simple email sending for ContactUs controller. Implement primitive email template.

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ContactUsRequest;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Mail\Message;
 
 class ContactUsController extends Controller
 {
@@ -15,8 +16,25 @@ class ContactUsController extends Controller
 
     public function send(ContactUsRequest $request): RedirectResponse
     {
+
         $data = $request->validated();
         \Log::debug('test', $data);
+
+        \Mail::send(
+            'emails.contactUs',
+            [
+                'email' => $data['email'],
+                'name' => $data['name'],
+                'districts' => $data['districts'],
+                'department' => $data['department'],
+                'messageText' => $data['message'],
+            ],
+            function (Message $message) use ($data) {
+                $message->subject('message from ' . $data['email']);
+                $message->to('tech@baloon.app');
+                $message->from('no-reply@baloon.app', 'Baloon mailer');
+            }
+        );
 
         return redirect()->route('contactUs')->withInput($data);
     }
