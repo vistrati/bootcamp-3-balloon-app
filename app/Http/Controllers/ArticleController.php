57e03b5e1cc751caf66eff74830f98f06494f41c
Egor Boaghi <e.boaghi@developmentaid.org>
2022-01-17 17:44:32 +0200

    Adjust ArticleController for ModelLogger

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,21 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use App\Services\ModelLogger;
 use Illuminate\Http\Request;
-use Psr\Log\LoggerInterface;
 
 class ArticleController extends Controller
 {
-    public function show($articleId, Request $request, LoggerInterface $logger)
+    public function show($articleId, Request $request, ModelLogger $logger)
     {
         $article = Article::findOrFail($articleId);
 
-        $user = $request->user();
-        $userRepresentation = $user ? "User with id {$user->id}" : "Unknown User";
-        $logger->info(
-            $userRepresentation . ' accessed ' . "article with id {$articleId}",
-            ['id' => $articleId, 'title' => $article->title],
-        );
+        $logger->logModel($request->user(), $article);
 
         return view('article.article', ['article' => $article]);
     }
